967c2114bb57476611d6953940b02080e86c88d3
Benjamin Stewart <benjuhminstewart@gmail.com>
2024-06-05 18:11:54 -0700

    fix: adjust emails to stop email cutoff

--- src/components/TerminalBoxContact.tsx
+++ src/components/TerminalBoxContact.tsx
@@ -22,17 +22,15 @@ export default function TerminalBoxContact() {
             <div className='terminal-sub-row'>
                 <a className='terminal-response-button link' href="mailto:benrstewart.12@gmail.com" onClick={() => sendEmail("benrstewart.12@gmail.com")}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 512" style={{ width: '1.5em', height: '1.5em', fill: "rgba(255,255,255,0.8)" }}>
-                        <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/>
+                        <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
                     </svg>
-                    <p className="terminal-sub-row-item">benrstewart.12@gmail.com</p>
-                    <p className="terminal-comment"># personal email</p>
+                    <p className="terminal-sub-row-item">personal email</p>
                 </a>
                 <a className='terminal-response-button link' href="mailto:stewartsoftwaresolutions@gmail.com" onClick={() => sendEmail("stewartsoftwaresolutions@gmail.com")}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 512" style={{ width: '1.5em', height: '1.5em', fill: "rgba(255,255,255,0.8)" }}>
-                        <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/>
+                        <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
                     </svg>
-                    <p className="terminal-sub-row-item">stewartsoftwaresolutions@gmail.com</p>
-                    <p className="terminal-comment"># consulting email</p>
+                    <p className="terminal-sub-row-item">consulting email</p>
                 </a>
             </div>
         )
@@ -45,9 +43,9 @@ export default function TerminalBoxContact() {
             <div className='terminal-sub-row'>
                 <button className='terminal-response-button'>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 512" style={{ width: '1.5em', height: '1.5em', marginRight: 10, fill: "rgba(255,255,255,0.8)" }}>
-                        <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/>
+                        <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
                     </svg>
-                    <p><span style={{color: "rgba(255,255,255,0.3)"}}>+1</span> (501) 291-0864</p>
+                    <p><span style={{ color: "rgba(255,255,255,0.3)" }}>+1</span> (501) 291-0864</p>
                     <p className="terminal-comment"># send a voicemail if you want to speak with me!</p>
                 </button>
             </div>
@@ -78,21 +76,21 @@ export default function TerminalBoxContact() {
                     </button>
                     <button className='terminal-response-button' onClick={() => setRenderingEmailOptions(!isRenderingEmailOptions)}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" style={{ width: '1.5em', height: '1.5em', marginRight: 10, fill: 'rgba(255,255,255,0.9)' }}>
-                            <path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM112 256H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/>
+                            <path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM112 256H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
 
                         </svg>
                         <p className='terminal-response-text'>
-                            email<span style={{color: "rgba(255,255,255,0.3)", fontFamily: "monospace"}}>.txt</span>
+                            email<span style={{ color: "rgba(255,255,255,0.3)", fontFamily: "monospace" }}>.txt</span>
                         </p>
                     </button>
                     {renderEmailOptions()}
                     <button className='terminal-response-button' onClick={() => setRenderingPhoneOptions(!isRenderingPhoneOptions)}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" style={{ width: '1.5em', height: '1.5em', marginRight: 10, fill: 'rgba(255,255,255,0.9)' }}>
-                            <path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM112 256H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/>
+                            <path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM112 256H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
 
                         </svg>
                         <p className='terminal-response-text'>
-                            phone<span style={{color: "rgba(255,255,255,0.3)", fontFamily: "monospace"}}>.txt</span>
+                            phone<span style={{ color: "rgba(255,255,255,0.3)", fontFamily: "monospace" }}>.txt</span>
                         </p>
                     </button>
                     {renderPhoneOptions()}
